9da4a5c889214c9c6c23a17ecb79c6806bff432a
Needlide <max.litvinyuck553@gmail.com>
2024-02-24 12:36:37 +0000

    Added function ListItem

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -3,13 +3,18 @@ package ua.edu.lntu.cw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw3.ui.theme.IPZ_CR_3Theme
 
 class MainActivity : ComponentActivity() {
@@ -30,17 +35,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainApp(modifier: Modifier = Modifier) {
+    LazyColumn{
+        items(20) {
+            _ -> Row(Modifier.padding(8.dp)){
+        }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    IPZ_CR_3Theme {
-        Greeting("Android")
+fun ListItem(modifier:Modifier = Modifier, message: String = "test", buttonMessage: String = "button") {
+    Text(message, modifier = modifier)
+    Button(onClick = { /*TODO*/ }, modifier = modifier) {
+        Text(text = buttonMessage)
     }
 }
